00c177bfb987b6a927be71ef07727162442066d1
Asma Farhana Mohideen Nazar <W0847949@myscc.ca>
2024-06-06 15:36:04 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RADGardens.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
